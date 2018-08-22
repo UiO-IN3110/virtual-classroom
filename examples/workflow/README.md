dc33a01fb6a3f65d1508dedd044738079279e4d0
Simon Funke <simon.funke@gmail.com>
2018-08-22 09:15:04 +0200

    Do not print out traceback in download_spreadsheet

--- examples/workflow/README.md
+++ examples/workflow/README.md
@@ -5,9 +5,9 @@
 ## Start of semester
 
 1. Edit `defaults_parameters.txt`.
-2. Ask students to fill out the Google Form https://docs.google.com/a/simula.no/forms/d/1JGSMC-9sRcuCanLF7MavHlGWVxuOjX-bwcIoyoM4yPE/edit
+2. Ask students to fill out the Google Form (see this template https://docs.google.com/a/simula.no/forms/d/1JGSMC-9sRcuCanLF7MavHlGWVxuOjX-bwcIoyoM4yPE/edit)
 3. Make sure the response Google Spreadsheet has the ordering: *Timestamp, Full name, UiO-Username, Username on Github, Email Address, Course*
-4. Download the student list with `python download_spreadsheet.py`
+4. Update the document name in `download_spreadsheet.py` and download the student list with `python download_spreadsheet.py`
 5. Mark all participating students with an `x` in `Attendance/students_base.txt`
 6. Create a github repository and team for each student with 
 
--- examples/workflow/download_spreadsheet.py
+++ examples/workflow/download_spreadsheet.py
@@ -1,10 +1,16 @@
 import virtual_classroom.utils as utils
 
 # Name of spreadsheet on Google sheets (You should change this to correct value)
-spreadsheet_name = "VirtualClassroomTest (Responses)"
+spreadsheet_name = "Sign up form for INF3331/INF43331 (2018) (Responses)"
 
 # Download parse, return contents and save it in default place
-csv_str = utils.download_google_spreadsheet(spreadsheet_name)  # Should prompt for login info if default not set
+try:
+    csv_str = utils.download_google_spreadsheet(spreadsheet_name)  # Should prompt for login info if default not set
+    utils.create_students_file_from_csv(csv_str=csv_str, output_filename="Attendance/students_base.txt")
+except Exception as e:
+    print("An error occured - exiting")
+    import sys
+    sys.exit()
+   
 
-utils.create_students_file_from_csv(csv_str=csv_str, output_filename="Attendance/students_base.txt")
 
